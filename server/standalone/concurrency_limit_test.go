792c96c5bfea2932d42f4522d59c5d0dd51e150c
Matthieu Blottière <49146345+mblottiere@users.noreply.github.com>
2021-03-12 09:25:23 +0100

    chore: test concurrency limiter to highlight how order is dismissed (#39)
    Requests will be processed one by one as expected, but their order of
    arrival won't be preserved.

--- server/standalone/concurrency_limit_test.go
+++ server/standalone/concurrency_limit_test.go
@@ -38,27 +38,31 @@ func TestBlockOnRequest(t *testing.T) {
 	}
 	unaryHandler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		defer wg.Done()
-		v, ok := req.(string)
-		if ok && v == "wait" {
+		v, ok := req.(int)
+		if ok && v == 1 {
 			_ = <-lock // Fake processing
 		}
-		done++
+		done = v
 
 		return "test", nil
 	}
 
-	wg.Add(2)
+	wg.Add(3)
 
 	// First request come in
-	go limiter.Intercept(context.TODO(), "wait", unaryInfo, unaryHandler)
+	go limiter.Intercept(context.TODO(), 1, unaryInfo, unaryHandler)
 
 	// Second request come in
-	go limiter.Intercept(context.TODO(), "test", unaryInfo, unaryHandler)
+	go limiter.Intercept(context.TODO(), 2, unaryInfo, unaryHandler)
+
+	// Third request come in
+	go limiter.Intercept(context.TODO(), 3, unaryInfo, unaryHandler)
 
 	assert.Equal(t, 0, done, "no request should have been processed")
 
 	lock <- new(struct{}) // first request finishes
 
-	wg.Wait() // Make sure goroutine has been processed
-	assert.Equal(t, 2, done, "first request should be done")
+	wg.Wait() // Make sure goroutines have been processed
+	// The concurrency limiter will make sure that requests are processed one by one REGARDLESS of their order of arrival
+	assert.GreaterOrEqual(t, done, 2, "last requests should be done")
 }
