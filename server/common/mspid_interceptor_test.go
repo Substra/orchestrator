e270585b648e902569910492dd1466ac6ac7b2bd
Aurélien Gasser <aurelien.gasser@gmail.com>
2021-03-16 13:59:56 -0400

    cleaner tests

--- server/common/mspid_interceptor_test.go
+++ server/common/mspid_interceptor_test.go
@@ -19,7 +19,6 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -46,86 +45,55 @@ func TestExtractMSPID(t *testing.T) {
 }
 
 func TestVerifyClientMSPID(t *testing.T) {
+
 	MSPID := "my-msp-id"
 
-	params := []struct {
-		peer          *peer.Peer
-		shouldSucceed bool
-		msg           string
-	}{
-		{
-			nil,
-			false,
-			"Certificate with a missing peer",
-		},
-		{
-			&peer.Peer{},
-			false,
-			"Certificate with an invalid peer",
-		},
-		{
-			&peer.Peer{AuthInfo: FakeTLSAuthInfo{}},
-			false,
-			"Certificate with an incorrect auth info type",
-		},
-		{
-			&peer.Peer{AuthInfo: credentials.TLSInfo{}},
-			false,
-			"Certificate with an empty TLS info",
-		},
-		{
-			&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{}}},
-			false,
-			"Certificate with an empty connection state",
-		},
-		{
-			&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{PeerCertificates: []*x509.Certificate{{}}}}},
-			false,
-			"Certificate with an empty certificate",
-		},
-		{
-			&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{PeerCertificates: []*x509.Certificate{{
-				Subject: pkix.Name{},
-			}}}}},
-			false,
-			"Certificate with an empty subject",
-		},
-		{
-			&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{PeerCertificates: []*x509.Certificate{{
-				Subject: pkix.Name{Organization: []string{}},
-			}}}}},
-			false,
-			"Certificate with an empty list of organizations",
-		},
-		{
-			&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{PeerCertificates: []*x509.Certificate{{
-				Subject: pkix.Name{Organization: []string{MSPID}},
-			}}}}},
-			true,
-			"Certificate with a valid MSPID",
-		},
-		{
-			&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{PeerCertificates: []*x509.Certificate{{
-				Subject: pkix.Name{Organization: []string{"some other mspid", MSPID}},
-			}}}}},
-			true,
-			"Certificate with both a valid MSPID and and invalid one",
-		},
-	}
-
-	for i := range params {
-		actual := testVerifyClientMSPID(t, MSPID, params[i].peer, params[i].shouldSucceed, params[i].msg)
-		shouldSucceed := params[i].shouldSucceed
-		prefix := "Should return a validation error"
-		if shouldSucceed {
-			prefix = "Should not return a validation error"
+	var verify = func(isValid bool, p *peer.Peer) func(*testing.T) {
+		return func(t *testing.T) {
+			ctx := context.TODO()
+			ctx = peer.NewContext(ctx, p)
+			err := VerifyClientMSPID(ctx, MSPID)
+			msg := "Should return a validation error"
+			if isValid {
+				msg = "Should not return a validation error"
+			}
+			assert.Equal(t, isValid, err == nil, msg)
 		}
-		assert.Equal(t, shouldSucceed, actual == nil, fmt.Sprintf("%v: %v", prefix, params[i].msg))
 	}
-}
 
-func testVerifyClientMSPID(t *testing.T, MSPID string, p *peer.Peer, shouldSucceed bool, msg string) error {
-	ctx := context.TODO()
-	ctx = peer.NewContext(ctx, p)
-	return VerifyClientMSPID(ctx, MSPID)
+	t.Run("Certificate with a missing peer", verify(false, nil))
+
+	t.Run("Certificate with an invalid peer", verify(false, &peer.Peer{}))
+
+	t.Run("Certificate with an incorrect auth info type", verify(false, &peer.Peer{AuthInfo: FakeTLSAuthInfo{}}))
+
+	t.Run("Certificate with an empty TLS info", verify(false, &peer.Peer{AuthInfo: credentials.TLSInfo{}}))
+
+	t.Run("Certificate with an empty connection state", verify(false,
+		&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{}}}))
+
+	t.Run("Certificate with an empty certificate", verify(false,
+		&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{
+			PeerCertificates: []*x509.Certificate{{}}},
+		}}))
+
+	t.Run("Certificate with an empty subject", verify(false,
+		&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{
+			PeerCertificates: []*x509.Certificate{{Subject: pkix.Name{}}}},
+		}}))
+
+	t.Run("Certificate with an empty list of organizations", verify(false,
+		&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{
+			PeerCertificates: []*x509.Certificate{{Subject: pkix.Name{Organization: []string{}}}}},
+		}}))
+
+	t.Run("Certificate with a valid MSPID", verify(true,
+		&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{
+			PeerCertificates: []*x509.Certificate{{Subject: pkix.Name{Organization: []string{MSPID}}}}},
+		}}))
+
+	t.Run("Certificate with both a valid MSPID and and invalid one", verify(true,
+		&peer.Peer{AuthInfo: credentials.TLSInfo{State: tls.ConnectionState{
+			PeerCertificates: []*x509.Certificate{{Subject: pkix.Name{Organization: []string{"other mspid", MSPID}}}}},
+		}}))
 }
