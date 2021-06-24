69834001a3736ab01ddf72ecb343a5adcfb4c03f
Matthieu Blottière <49146345+mblottiere@users.noreply.github.com>
2021-06-24 09:29:18 +0200

    chore: remove license header (#247)
    License is applied globally to every file of the repo.

--- lib/doc.go
+++ lib/doc.go
@@ -1,17 +1,3 @@
-// Copyright 2020 Owkin Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 // Package lib defines structures and business logic related to substra orchestration platform.
 // This package does not rely on a concrete storage backend, but rather defines a persistence interface.
 // Business logic should be agnostic of the backend as well, as it may be called from either a gRPC server
--- lib/event/dispatcher.go
+++ lib/event/dispatcher.go
@@ -1,17 +1,3 @@
-// Copyright 2021 Owkin Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 // Package event defines orchestration Event structure and interfaces to interact with it.
 // Events are emitted by orchestration logic and are placed in a Queue.
 // Once the orchestration action is done, those events can be sent to a broker through a Dispatcher.
