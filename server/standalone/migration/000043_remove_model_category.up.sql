2d4237351176bb771f8d2ef82ed53e4416f9792f
Matthieu Blottière <matthieu.blottiere@owkin.com>
2022-09-02 17:12:17 +0200

    fix: migration
    Signed-off-by: Matthieu Blottière <matthieu.blottiere@owkin.com>

--- server/standalone/migration/000043_remove_model_category.up.sql
+++ server/standalone/migration/000043_remove_model_category.up.sql
@@ -1,6 +1,20 @@
-ALTER TABLE models DROP COLUMN category CASCADE;
-DROP TABLE model_categories;
+SELECT execute($$
+    ALTER TABLE models DROP COLUMN category CASCADE;
+    DROP TABLE model_categories;
 
-UPDATE events
-SET asset = asset #- '{category}'
-WHERE asset_kind = 'ASSET_MODEL';
+    CREATE VIEW expanded_models AS
+    SELECT key,
+           compute_task_key,
+           address,
+           a.checksum AS checksum,
+           permissions,
+           owner,
+           channel,
+           creation_date
+    FROM models
+    LEFT JOIN addressables a ON models.address = a.storage_address;
+
+    UPDATE events
+    SET asset = asset #- '{category}'
+    WHERE asset_kind = 'ASSET_MODEL';
+$$) WHERE table_exists('public', 'model_categories');
