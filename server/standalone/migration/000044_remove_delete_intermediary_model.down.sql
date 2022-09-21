5334422731355d8d7da8d55c77e36c5782e0f593
Alexandre-P <alexandre.picosson@owkin.com>
2022-09-21 15:02:15 +0200

    chore: migration
    Signed-off-by: Alexandre-P <alexandre.picosson@owkin.com>

--- server/standalone/migration/000044_remove_delete_intermediary_model.down.sql
+++ server/standalone/migration/000044_remove_delete_intermediary_model.down.sql
@@ -1,8 +1,5 @@
 ALTER TABLE compute_plans
-ADD COLUMN IF NOT EXISTS delete_intermediary_models bool;
-
-ALTER TABLE compute_plans
-ALTER COLUMN delete_intermediary_models SET DEFAULT false;
+ADD COLUMN IF NOT EXISTS delete_intermediary_models bool DEFAULT false;
 
 ALTER TABLE compute_plans
 ALTER COLUMN delete_intermediary_models SET NOT NULL; 
