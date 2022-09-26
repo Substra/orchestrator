43085ed3497a82db64dbd1b8be6fdd5e9d483e71
Alexandre-P <alexandre.picosson@owkin.com>
2022-09-26 10:05:36 +0200

    chore: migrate events
    Signed-off-by: Alexandre-P <alexandre.picosson@owkin.com>

--- server/standalone/migration/000045_remove_delete_intermediary_model.down.sql
+++ server/standalone/migration/000045_remove_delete_intermediary_model.down.sql
@@ -3,3 +3,7 @@ ADD COLUMN IF NOT EXISTS delete_intermediary_models bool DEFAULT false;
 
 ALTER TABLE compute_plans
 ALTER COLUMN delete_intermediary_models SET NOT NULL; 
+
+UPDATE events
+SET asset = asset::jsonb || '{"delete_intermediary_models": false}'::jsonb
+WHERE asset_kind = 'ASSET_COMPUTE_PLAN';
