c8443a6d8c076f6f92852a3b7efd99dfbfc9ec25
Thibaud Martinez <thibaud.martinez@owkin.com>
2022-09-27 10:01:58 +0200

    set defaults using constants
    Signed-off-by: Thibaud Martinez <thibaud.martinez@owkin.com>

--- server/common/keepalive.go
+++ server/common/keepalive.go
@@ -5,11 +5,16 @@ import (
 	"google.golang.org/grpc/keepalive"
 )
 
+const (
+	defaultMinTime             = "30s"
+	defaultPermitWithoutStream = "false"
+)
+
 // GetKeepAliveOptions will return server option with gRPC keepalive setup.
 // This may panic on missing or invalid configuration env var.
 func GetKeepAliveOptions() grpc.ServerOption {
-	minTime := MustParseDuration(GetEnvOrFallback("GRPC_KEEPALIVE_POLICY_MIN_TIME", "30s"))
-	permitWithoutStream := MustParseBool(GetEnvOrFallback("GRPC_KEEPALIVE_POLICY_PERMIT_WITHOUT_STREAM", "false"))
+	minTime := MustParseDuration(GetEnvOrFallback("GRPC_KEEPALIVE_POLICY_MIN_TIME", defaultMinTime))
+	permitWithoutStream := MustParseBool(GetEnvOrFallback("GRPC_KEEPALIVE_POLICY_PERMIT_WITHOUT_STREAM", defaultPermitWithoutStream))
 
 	return grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
 		MinTime:             minTime,
