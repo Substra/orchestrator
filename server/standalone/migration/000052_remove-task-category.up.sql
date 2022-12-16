69a3d33119b3279d05c3356d6db5924cd33c8449
Guilhem Barthes <guilhem.barthes@owkin.com>
2022-12-16 16:50:41 +0100

    fix: Apply feedback
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- server/standalone/migration/000052_remove-task-category.up.sql
+++ server/standalone/migration/000052_remove-task-category.up.sql
@@ -24,6 +24,6 @@ SELECT execute($$
     ALTER TABLE compute_tasks DROP COLUMN category;
     DROP TABLE compute_task_categories;
 
-    UPDATE events SET asset = asset #- '{category}'
+    UPDATE events SET asset = asset - 'category'
     WHERE asset_kind = 'ASSET_COMPUTE_TASK';
 $$) WHERE column_exists('public', 'compute_tasks', 'category');
