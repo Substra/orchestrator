4b90948e925ee06e683c5805b158701662145820
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-01-02 13:46:18 +0100

    fix: Typo in migration 52
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- server/standalone/migration/000052_remove-parent_task_key-in-compute-task.up.sql
+++ server/standalone/migration/000052_remove-parent_task_key-in-compute-task.up.sql
@@ -1,6 +1,6 @@
 SELECT execute($$
     DROP VIEW IF EXISTS expanded_compute_tasks;
     UPDATE events
-    SET asset = asset #- '{parent_task_keys}'
+    SET asset = asset - 'parent_task_keys'
     WHERE asset_kind = 'ASSET_COMPUTE_TASK';
 $$) WHERE view_exists('public', 'expanded_compute_tasks');
