daa912eade851f6d53e5e99ade818bac44114423
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-01-02 15:32:56 +0100

    fix: replace parent_task_keys by parentTaskKeys
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- server/standalone/migration/000052_remove-parent_task_key-in-compute-task.up.sql
+++ server/standalone/migration/000052_remove-parent_task_key-in-compute-task.up.sql
@@ -1,4 +1,6 @@
-DROP VIEW IF EXISTS expanded_compute_tasks;
-UPDATE events
-SET asset = asset - 'parent_task_keys'
-WHERE asset_kind = 'ASSET_COMPUTE_TASK';
+SELECT execute($$
+    DROP VIEW IF EXISTS expanded_compute_tasks;
+    UPDATE events
+    SET asset = asset - 'parentTaskKeys'
+    WHERE asset_kind = 'ASSET_COMPUTE_TASK';
+$$) WHERE view_exists('public', 'expanded_compute_tasks');
