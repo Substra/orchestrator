35f8a0545a2eba7778790928566bf23de2637f22
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-01-02 13:53:57 +0100

    fix: drop condition in migration 52
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- server/standalone/migration/000052_remove-parent_task_key-in-compute-task.up.sql
+++ server/standalone/migration/000052_remove-parent_task_key-in-compute-task.up.sql
@@ -1,6 +1,4 @@
-SELECT execute($$
-    DROP VIEW IF EXISTS expanded_compute_tasks;
-    UPDATE events
-    SET asset = asset - 'parent_task_keys'
-    WHERE asset_kind = 'ASSET_COMPUTE_TASK';
-$$) WHERE view_exists('public', 'expanded_compute_tasks');
+DROP VIEW IF EXISTS expanded_compute_tasks;
+UPDATE events
+SET asset = asset - 'parent_task_keys'
+WHERE asset_kind = 'ASSET_COMPUTE_TASK';
