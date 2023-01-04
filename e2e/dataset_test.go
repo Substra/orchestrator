575910700175b163b9f194302957af5e6ae788ef
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-01-04 15:30:40 +0100

    rm test only
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- e2e/dataset_test.go
+++ e2e/dataset_test.go
@@ -16,11 +16,9 @@ func TestDatasetSampleKeys(t *testing.T) {
 	appClient.RegisterDataManager(client.DefaultDataManagerOptions())
 	appClient.RegisterDataSample(client.DefaultDataSampleOptions().WithKeyRef("ds1"))
 	appClient.RegisterDataSample(client.DefaultDataSampleOptions().WithKeyRef("ds2"))
-	appClient.RegisterDataSample(client.DefaultDataSampleOptions().WithTestOnly(true).WithKeyRef("testds"))
 
 	dataset := appClient.GetDataset(client.DefaultDataManagerRef)
 
-	require.Equal(t, 1, len(dataset.TestDataSampleKeys), "dataset should contain a single test sample")
-	require.Equal(t, 2, len(dataset.TrainDataSampleKeys), "dataset should contain 2 train samples")
-	require.Equal(t, appClient.GetKeyStore().GetKey("testds"), dataset.TestDataSampleKeys[0], "dataset should contain valid test sample ID")
+	require.Equal(t, 1, len(dataset.TestDataSampleKeys), "dataset should contain a single data sample")
+	require.Equal(t, 2, len(dataset.TrainDataSampleKeys), "dataset should contain 2 data samples")
 }
