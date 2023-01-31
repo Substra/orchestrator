2b3cacdb55a8520bab6fce1617dd7c2cb67551f4
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-01-31 18:20:27 +0100

    migration
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- server/standalone/migration/000055_rename-algo-to-function.up.sql
+++ server/standalone/migration/000055_rename-algo-to-function.up.sql
@@ -19,8 +19,6 @@ RENAME COLUMN algo_key TO function_key;
 ALTER TABLE algo_inputs
 RENAME TO function_inputs;
 
-DROP VIEW IF EXISTS expanded_compute_tasks;
-
 DROP VIEW IF EXISTS expanded_algos;
 CREATE VIEW expanded_functions AS
 SELECT 	key,
@@ -38,24 +36,10 @@ FROM functions
 JOIN addressables desc_add ON functions.description = desc_add.storage_address
 JOIN addressables function_add ON functions.functionAdress = function_add.storage_address;
 
-CREATE VIEW expanded_compute_tasks AS
-SELECT t.key AS key,
-       t.channel AS channel,
-       t.compute_plan_key AS compute_plan_key,
-       t.status AS status,
-       t.worker AS worker,
-       t.owner as owner,
-       t.rank AS rank,
-       t.creation_date AS creation_date,
-       t.logs_permission AS logs_permission,
-       t.metadata AS metadata,
-       t.function_key AS function_key,
-       COALESCE(p.parent_task_keys, '[]'::jsonb) AS parent_task_keys
-FROM compute_tasks t
-        LEFT JOIN (
-    SELECT child_task_key, JSONB_AGG(parent_task_key) AS parent_task_keys
-    FROM compute_task_parents
-    GROUP BY child_task_key
-) p ON p.child_task_key = t.key;
-
-
+UPDATE events e
+SET asset = JSON_MODIFY(
+    JSON_MODIFY(asset,'$.function', JSON_VALUE(asset,'$.algo')),
+    '$.algo',
+    NULL
+    )
+WHERE asset_kind = 'ASSET_COMPUTE_TASK';
