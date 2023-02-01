004c4c44824ee72b02abcb51970a90d60ba46463
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-02-01 11:06:25 +0100

    migration
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- server/standalone/migration/000055_rename-algo-to-function.up.sql
+++ server/standalone/migration/000055_rename-algo-to-function.up.sql
@@ -1,5 +1,7 @@
 ALTER TABLE algos
-RENAME CONSTRAINT algos_owner_channel_fkey TO functions_owner_channel_fkey
+RENAME CONSTRAINT algos_owner_channel_fkey TO functions_owner_channel_fkey;
+
+ALTER TABLE algos
 RENAME COLUMN algorithm TO functionAdress;
 
 ALTER TABLE algos
@@ -21,7 +23,9 @@ ALTER TABLE algo_inputs
 RENAME TO function_inputs;
 
 ALTER TABLE performances
-RENAME COLUMN algo_key TO function_key
+RENAME COLUMN algo_key TO function_key;
+
+ALTER TABLE performances
 RENAME CONSTRAINT performances_algo_key_fkey TO performances_function_key_fkey;
 
 DROP VIEW IF EXISTS expanded_algos;
