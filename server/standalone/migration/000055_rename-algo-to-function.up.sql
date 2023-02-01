40c8862e8f6477aa25552849c9a6c4cff557c030
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-02-01 15:12:41 +0100

    migration
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- server/standalone/migration/000055_rename-algo-to-function.up.sql
+++ server/standalone/migration/000055_rename-algo-to-function.up.sql
@@ -56,13 +56,13 @@ DELETE FROM asset_kinds
 WHERE kind = 'ASSET_ALGO';
 
 UPDATE events
-SET asset = JSONB_SET(COALESCE(asset,
-                      '{function}'),
-                      asset -> 'algo') #- '{algo}'
+SET asset = JSONB_SET(asset  #- '{algo}',
+                      '{function}',
+                      asset -> 'algo')
 WHERE asset_kind = 'ASSET_COMPUTE_TASK';
 
 UPDATE events
-SET asset = JSONB_SET(COALESCE(asset,
-                      '{function,function}'),
-                      asset -> 'function'-> 'algorithm') #- '{function,algorithm}'
+SET asset = JSONB_SET(asset #- '{function,algorithm}',
+                      '{function,function}',
+                      asset -> 'function'-> 'algorithm')
 WHERE asset_kind = 'ASSET_COMPUTE_TASK';
