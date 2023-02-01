314dc3f4bfbd54c8fcdb108687269d1a6fa6cd70
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-02-01 09:59:59 +0100

    migration
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- server/standalone/migration/000055_rename-algo-to-function.up.sql
+++ server/standalone/migration/000055_rename-algo-to-function.up.sql
@@ -36,6 +36,16 @@ FROM functions
 JOIN addressables desc_add ON functions.description = desc_add.storage_address
 JOIN addressables function_add ON functions.functionAdress = function_add.storage_address;
 
+INSERT INTO asset_kinds(kind)
+VALUES ('ASSET_FUNCTION');
+
+UPDATE events e
+SET asset_kind = 'ASSET_FUNCTION'
+WHERE e.asset_kind = 'ASSET_ALGO';
+
+DELETE FROM asset_kinds
+WHERE kind = 'ASSET_ALGO';
+
 UPDATE events
-set asset = asset - 'algo' || JSONB_BUILD_OBJECT('function', asset->'algo')
+SET asset = asset - 'algo' || JSONB_BUILD_OBJECT('function', asset->'algo')
 WHERE asset_kind = 'ASSET_COMPUTE_TASK';
