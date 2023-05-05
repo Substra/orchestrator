1a4fdbfc2888850e3db518de52eb5a72337fedea
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-05-05 16:54:39 +0200

    add comments
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- server/standalone/migration/000056_performance-primary-key.up.sql
+++ server/standalone/migration/000056_performance-primary-key.up.sql
@@ -1,4 +1,8 @@
 SELECT execute($$
+        /* To keep differiantiability regarding the performances, we update the output identifier to the metric name */
+        /* on previous performances */
+
+        /* Delete constraints regarding the identifier */
         ALTER TABLE performances
         DROP CONSTRAINT performances_pkey;
 
@@ -11,9 +15,7 @@ SELECT execute($$
         ALTER TABLE compute_task_output_assets
         DROP CONSTRAINT compute_task_output_assets_compute_task_key_compute_task_o_fkey;
 
-        ALTER TABLE performances
-        ADD COLUMN compute_task_output_identifier varchar(100);
-
+        /* Set identifier to metric name on the concerned compute task output */
         UPDATE compute_task_outputs cto
         SET identifier = f.name
         FROM functions f, events e
@@ -21,6 +23,7 @@ SELECT execute($$
         AND f.key = (e.asset ->> 'metricKey')::uuid
         AND cto.compute_task_key = (e.asset ->> 'computeTaskKey')::uuid;
 
+        /* Set identifier to metric name on the concerned compute task output asset */
         UPDATE compute_task_output_assets ctoa
         SET compute_task_output_identifier = cto.identifier
         FROM compute_task_outputs cto, events e
@@ -28,11 +31,17 @@ SELECT execute($$
         AND cto.compute_task_key=ctoa.compute_task_key
         AND cto.compute_task_key = (e.asset ->> 'computeTaskKey')::uuid;
 
+        /* Create an identifier column on performances */
+        ALTER TABLE performances
+        ADD COLUMN compute_task_output_identifier varchar(100);
+
+        /* Set identifier to the right value */
         UPDATE performances p
         SET compute_task_output_identifier = cto.identifier
         FROM compute_task_outputs cto
         WHERE cto.compute_task_key=p.compute_task_key;
 
+        /* Set constraints back */
         ALTER TABLE performances
         ALTER COLUMN compute_task_output_identifier SET NOT NULL;
 
@@ -50,6 +59,7 @@ SELECT execute($$
         ALTER TABLE compute_task_output_assets
         ADD PRIMARY KEY(compute_task_key, compute_task_output_identifier, position);
 
+        /* Update asset */
         UPDATE events e
         SET asset = jsonb_set(asset, '{computeTaskOutputIdentifier}', to_jsonb(cto.identifier))
         FROM compute_task_outputs cto
