940e8c41306f89d53ccf9a32af93e72e156531d4
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-05-09 17:41:51 +0200

    remove metric
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- lib/service/performance_test.go
+++ lib/service/performance_test.go
@@ -1,41 +1,28 @@
 package service
 
 import (
-	"errors"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/substra/orchestrator/lib/asset"
-	orcerrors "github.com/substra/orchestrator/lib/errors"
 	"github.com/substra/orchestrator/lib/persistence"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func TestRegisterPerformance(t *testing.T) {
 	dbal := new(persistence.MockDBAL)
-	as := new(MockFunctionAPI)
 	cts := new(MockComputeTaskAPI)
 	es := new(MockEventAPI)
 	ts := new(MockTimeAPI)
 	provider := newMockedProvider()
 	provider.On("GetComputeTaskService").Return(cts)
-	provider.On("GetFunctionService").Return(as)
 	provider.On("GetPerformanceDBAL").Return(dbal)
 	provider.On("GetEventService").Return(es)
 	provider.On("GetTimeService").Return(ts)
 	service := NewPerformanceService(provider)
 
 	ts.On("GetTransactionTime").Once().Return(time.Unix(1337, 0))
-
-	metric := &asset.Function{
-		Key: "1da600d4-f8ad-45d7-92a0-7ff752a82275",
-		Outputs: map[string]*asset.FunctionOutput{
-			"auc": {
-				Kind: asset.AssetKind_ASSET_PERFORMANCE,
-			},
-		}}
-	as.On("GetFunction", "1da600d4-f8ad-45d7-92a0-7ff752a82275").Return(metric, nil)
 
 	task := &asset.ComputeTask{
 		Key:    "taskTest",
@@ -44,7 +31,7 @@ func TestRegisterPerformance(t *testing.T) {
 		Outputs: map[string]*asset.ComputeTaskOutput{
 			"auc": {},
 		},
-		FunctionKey: metric.Key,
+		FunctionKey: "1da600d4-f8ad-45d7-92a0-7ff752a82275",
 	}
 	cts.On("GetTask", "08680966-97ae-4573-8b2d-6c4db2b3c532").Return(task, nil)
 
@@ -85,7 +72,6 @@ func TestRegisterPerformance(t *testing.T) {
 	_, err := service.RegisterPerformance(perf, "test")
 	assert.NoError(t, err)
 
-	as.AssertExpectations(t)
 	cts.AssertExpectations(t)
 	provider.AssertExpectations(t)
 	ts.AssertExpectations(t)
@@ -111,55 +97,6 @@ func TestRegisterPerformanceInvalidTask(t *testing.T) {
 	_, err := service.RegisterPerformance(perf, "test")
 	assert.Error(t, err)
 
-	cts.AssertExpectations(t)
-	provider.AssertExpectations(t)
-}
-
-func TestRegisterPerformanceInvalidOutput(t *testing.T) {
-	as := new(MockFunctionAPI)
-	cts := new(MockComputeTaskAPI)
-	provider := newMockedProvider()
-	provider.On("GetComputeTaskService").Return(cts)
-	provider.On("GetFunctionService").Return(as)
-	service := NewPerformanceService(provider)
-
-	metric := &asset.Function{
-		Key: "1da600d4-f8ad-45d7-92a0-7ff752a82275",
-		Outputs: map[string]*asset.FunctionOutput{
-			"auc": {
-				Kind: asset.AssetKind_ASSET_UNKNOWN,
-			},
-		}}
-	as.On("GetFunction", "1da600d4-f8ad-45d7-92a0-7ff752a82275").Return(metric, nil)
-
-	task := &asset.ComputeTask{
-		Status: asset.ComputeTaskStatus_STATUS_DOING,
-		Worker: "test",
-		Outputs: map[string]*asset.ComputeTaskOutput{
-			"auc": {},
-		},
-		FunctionKey: metric.Key,
-	}
-	cts.On("GetTask", "08680966-97ae-4573-8b2d-6c4db2b3c532").Return(task, nil)
-
-	perf := &asset.NewPerformance{
-		ComputeTaskKey:              "08680966-97ae-4573-8b2d-6c4db2b3c532",
-		ComputeTaskOutputIdentifier: "foo",
-		PerformanceValue:            0.36492,
-	}
-
-	_, err := service.RegisterPerformance(perf, "test")
-	assert.ErrorContains(t, err, "has no output named \"foo\"")
-	orcError := new(orcerrors.OrcError)
-	assert.True(t, errors.As(err, &orcError))
-	assert.Equal(t, orcerrors.ErrMissingTaskOutput, orcError.Kind)
-
-	perf.ComputeTaskOutputIdentifier = "auc"
-	_, err = service.RegisterPerformance(perf, "test")
-	orcError = new(orcerrors.OrcError)
-	assert.True(t, errors.As(err, &orcError))
-	assert.Equal(t, orcerrors.ErrIncompatibleKind, orcError.Kind)
-	as.AssertExpectations(t)
 	cts.AssertExpectations(t)
 	provider.AssertExpectations(t)
 }
