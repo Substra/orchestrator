79db28e95d3a1bfb73039b57695c65ae500e207e
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-09-05 13:40:36 +0200

    feat: propagate function failure
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- lib/service/failure_report.go
+++ lib/service/failure_report.go
@@ -35,6 +35,7 @@ func NewFailureReportService(provider FailureReportDependencyProvider) *FailureR
 	return &FailureReportService{provider}
 }
 
+
 func (s *FailureReportService) RegisterFailureReport(newFailureReport *asset.NewFailureReport, requester string) (*asset.FailureReport, error) {
 	s.GetLogger().Debug().Interface("failureReport", newFailureReport).Str("requester", requester).Msg("Registering new failure report")
 
@@ -42,37 +43,17 @@ func (s *FailureReportService) RegisterFailureReport(newFailureReport *asset.New
 	if err != nil {
 		return nil, errors.FromValidationError(asset.FailureReportKind, err)
 	}
-
 	switch newFailureReport.AssetType {
-	case asset.FailedAssetKind_FAILED_ASSET_COMPUTE_TASK:
-		task, err := s.GetComputeTaskService().GetTask(newFailureReport.AssetKey)
-		if err != nil {
-			return nil, err
-		}
+		case asset.FailedAssetKind_FAILED_ASSET_COMPUTE_TASK:
+			err = s.processTaskFailure(newFailureReport.AssetKey, requester)
+		case asset.FailedAssetKind_FAILED_ASSET_FUNCTION:
+			err = s.processFunctionFailure(newFailureReport.AssetKey, requester)
+		default:
+			return nil, errors.NewBadRequest("can only register failure for asset_kind values function and compute task")
+	}
 
-		if task.Worker != requester {
-			return nil, errors.NewPermissionDenied(fmt.Sprintf("only %q worker can register failure report for compute task", task.Worker))
-		}
-
-		if task.Status != asset.ComputeTaskStatus_STATUS_DOING {
-			return nil, errors.NewBadRequest(fmt.Sprintf("cannot register failure report for task with status %q", task.Status.String()))
-		}
-
-		err = s.GetComputeTaskService().ApplyTaskAction(task.Key, asset.ComputeTaskAction_TASK_ACTION_FAILED, "failure report registered", requester)
-		if err != nil {
-			return nil, err
-		}
-	case asset.FailedAssetKind_FAILED_ASSET_FUNCTION:
-		function, err := s.GetFunctionService().GetFunction(newFailureReport.AssetKey)
-		if err != nil {
-			return nil, err
-		}
-
-		if function.Owner != requester {
-			return nil, errors.NewPermissionDenied(fmt.Sprintf("only %q owner can register failure report for function", function.Owner))
-		}
-	default:
-		return nil, errors.NewBadRequest("can only register failure for asset_kind values function and compute task")
+	if err != nil {
+		return nil, err
 	}
 
 	failureReport := &asset.FailureReport{
@@ -106,4 +87,54 @@ func (s *FailureReportService) RegisterFailureReport(newFailureReport *asset.New
 func (s *FailureReportService) GetFailureReport(assetKey string) (*asset.FailureReport, error) {
 	s.GetLogger().Debug().Str("assetKey", assetKey).Msg("Get failure report")
 	return s.GetFailureReportDBAL().GetFailureReport(assetKey)
+}
+
+
+func checkTaskPermissions(task *asset.ComputeTask, requester string) error {
+	if task.Worker != requester {
+		return errors.NewPermissionDenied(fmt.Sprintf("only %q worker can register failure report for compute task", task.Worker))
+	}
+
+	if task.Status != asset.ComputeTaskStatus_STATUS_DOING {
+		return errors.NewBadRequest(fmt.Sprintf("cannot register failure report for task with status %q", task.Status.String()))
+	}
+
+	return nil
+}
+
+func (s *FailureReportService) processTaskFailure(taskKey string, requester string) error {
+	task, err := s.GetComputeTaskService().GetTask(taskKey)
+	if err != nil {
+		return err
+	}
+
+	err = checkTaskPermissions(task, requester)
+	if err != nil {
+		return err
+	}
+
+	return s.GetComputeTaskService().ApplyTaskAction(taskKey, asset.ComputeTaskAction_TASK_ACTION_FAILED, "failure report registered", requester)
+}
+
+func checkFunctionPermissions(function *asset.Function, requester string) error {
+	if function.Owner != requester {
+		return errors.NewPermissionDenied(fmt.Sprintf("only %q owner can register failure report for function", function.Owner))
+	}
+
+	return nil
+}
+
+func (s *FailureReportService) processFunctionFailure(functionKey string, requester string) error {
+	function, err := s.GetFunctionService().GetFunction(functionKey)
+	if err != nil {
+		return err
+	}
+
+	err = checkFunctionPermissions(function, requester)
+
+	if err != nil {
+		return err
+	}
+
+	return s.GetFunctionService().ApplyFunctionAction(functionKey, asset.FunctionAction_FUNCTION_ACTION_FAILED, "failure report registered", requester)
 }
