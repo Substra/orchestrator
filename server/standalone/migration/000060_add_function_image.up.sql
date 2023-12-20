a63421f92a2bff7035a1971f7a843faf42cf630a
ThibaultFy <thibault.fouqueray@gmail.com>
2023-12-20 14:58:30 +0100

    chore: add column empty to addressables
    Signed-off-by: ThibaultFy <thibault.fouqueray@gmail.com>

--- server/standalone/migration/000060_add_function_image.up.sql
+++ server/standalone/migration/000060_add_function_image.up.sql
@@ -5,6 +5,8 @@ SELECT execute($$
     ALTER TABLE functions
     RENAME COLUMN functionAddress to archive_address;
 
+    INSERT INTO addressables(storage_address, checksum) VALUES ('','');
+
     DROP VIEW IF EXISTS expanded_functions;
     CREATE VIEW expanded_functions AS
         SELECT 	key,
@@ -24,7 +26,7 @@ SELECT execute($$
         FROM functions
         JOIN addressables desc_add ON functions.description = desc_add.storage_address
         JOIN addressables archive_add ON functions.archive_address = archive_add.storage_address
-        LEFT JOIN addressables image_add ON functions.image_address = image_add.storage_address;
+        JOIN addressables image_add ON functions.image_address = image_add.storage_address;
 
     UPDATE events
     SET asset = jsonb_set(asset, '{archive}', asset->'function') - 'function'
