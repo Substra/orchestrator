4c2a0cb94817204ca583e89c070602a0e85055ef
ThibaultFy <thibault.fouqueray@gmail.com>
2024-03-22 16:20:26 +0100

    fix migration
    Signed-off-by: ThibaultFy <thibault.fouqueray@gmail.com>

--- server/standalone/migration/000063_delete_type_from_datamanager.sql
+++ server/standalone/migration/000063_delete_type_from_datamanager.sql
@@ -2,7 +2,7 @@ SELECT execute($$
     ALTER TABLE datamanagers
     DROP COLUMN type;
 
-    UPDATE events SET asset = asset - 'testOnly'
+    UPDATE events SET asset = asset - 'type'
     WHERE asset_kind = 'ASSET_DATA_MANAGER';
 
 $$) WHERE column_exists('public', 'datamanagers', 'type');
