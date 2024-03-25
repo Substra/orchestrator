f3e53cf57f49c97ce71b2bc3d0651fb1d2861b3c
ThibaultFy <thibault.fouqueray@gmail.com>
2024-03-25 15:59:02 +0100

    update migration
    Signed-off-by: ThibaultFy <thibault.fouqueray@gmail.com>

--- server/standalone/migration/000063_delete_type_from_datamanager.sql
+++ server/standalone/migration/000063_delete_type_from_datamanager.sql
@@ -1,4 +1,23 @@
 SELECT execute($$
+    DROP VIEW IF EXISTS expanded_datamanagers;
+    CREATE VIEW expanded_datamanagers AS
+    SELECT key,
+           name,
+           owner,
+           channel,
+           permissions,
+           description         AS description_address,
+           desc_add.checksum   AS description_checksum,
+           opener              AS opener_address,
+           opener_add.checksum AS opener_checksum,
+           type,
+           creation_date,
+           logs_permission,
+           metadata
+    FROM datamanagers
+    JOIN addressables desc_add ON datamanagers.description = desc_add.storage_address
+    JOIN addressables opener_add ON datamanagers.opener = opener_add.storage_address;
+
     ALTER TABLE datamanagers
     DROP COLUMN type;
 
