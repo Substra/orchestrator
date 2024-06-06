31b751f985b46051b0b4568e7c45653d637c02ea
ThibaultFy <thibault.fouqueray@gmail.com>
2024-06-06 16:23:07 +0200

    chore: transitionCanceled if compute plan is terminated
    Signed-off-by: ThibaultFy <thibault.fouqueray@gmail.com>

--- lib/service/computetaskstate.go
+++ lib/service/computetaskstate.go
@@ -135,7 +135,15 @@ func (s *ComputeTaskService) applyTaskAction(task *asset.ComputeTask, action ass
 	case asset.ComputeTaskAction_TASK_ACTION_FAILED:
 		transition = transitionFailed
 	case asset.ComputeTaskAction_TASK_ACTION_DONE:
-		transition = transitionDone
+		plan, err := s.GetComputePlanService().GetPlan(task.ComputePlanKey)
+		if err != nil {
+			return err
+		}
+		if plan.IsTerminated() {
+			transition = transitionCanceled
+		} else {
+			transition = transitionDone
+		}
 	case asset.ComputeTaskAction_TASK_ACTION_BUILD_STARTED:
 		transition = transitionBuilding
 	case asset.ComputeTaskAction_TASK_ACTION_BUILD_FINISHED:
