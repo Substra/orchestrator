5db496723c78bfbf4d67966fd849762b719a70ad
Guilhem Barthés <guilhem.barthes@owkin.com>
2024-11-19 16:49:19 +0100

    fix: renew skaffold certificate extend duration (#477)
    ## Description
    Fixes RP-1332
    
    ## How has this been tested?
    
    <!-- Please describe the tests that you ran to verify your changes.  -->
    
    ## Checklist
    
    - [ ] [changelog](../CHANGELOG.md) was updated with notable changes
    - [ ] documentation was updated
    
    ---------
    
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>

--- examples/tools/certificates.sh
+++ examples/tools/certificates.sh
@@ -4,15 +4,15 @@
 # Call this function to generate a new CA cert/key pair instead.
 function generate_cacert {
     openssl genrsa -out ca.key 2048
-    openssl req -new -x509 -days 365 -sha256 -key ca.key -extensions v3_ca -config openssl-with-ca.cnf -subj "/C=FR/ST=Loire-Atlantique/L=Nantes/O=Orchestrator Root CA/CN=Orchestrator Root CA" -out ca.crt
+    # 18225 days = 5 years = 5 * 365
+    openssl req -new -x509 -days 1825 -sha256 -key ca.key -extensions v3_ca -config openssl-with-ca.cnf -subj "/C=FR/ST=Loire-Atlantique/L=Nantes/O=Orchestrator Root CA/CN=Orchestrator Root CA" -out ca.crt
 }
 
 function generate_new_k8s_cacert {
-    kubectl -n "org-1" create configmap orchestrator-tls-cacert --from-file=ca.crt --dry-run=client --output=yaml > "cm-tls-org-1-cacert.yaml"
-    kubectl -n "org-2" create configmap orchestrator-tls-cacert --from-file=ca.crt --dry-run=client --output=yaml > "cm-tls-org-2-cacert.yaml"
-    kubectl -n "org-3" create configmap orchestrator-tls-cacert --from-file=ca.crt --dry-run=client --output=yaml > "cm-tls-org-3-cacert.yaml"
+    kubectl -n "org-1" create secret generic orchestrator-tls-cacert --from-file=ca.crt --dry-run=client --output=yaml > "secret-tls-org-1-cacert.yaml"
+    kubectl -n "org-2" create secret generic orchestrator-tls-cacert --from-file=ca.crt --dry-run=client --output=yaml > "secret-tls-org-2-cacert.yaml"
+    kubectl -n "org-3" create secret generic orchestrator-tls-cacert --from-file=ca.crt --dry-run=client --output=yaml > "secret-tls-org-3-cacert.yaml"
     kubectl -n "cert-manager" create secret tls orchestrator-tls-ca --key="ca.key" --cert="ca.crt" --dry-run=client --output=yaml > "secret-cacert-certmanager.yaml"
-    echo "Don't forget to update the \"orchestrator-tls-cacert\" ConfigMaps in substra-backend"
 }
 
 generate_cacert
